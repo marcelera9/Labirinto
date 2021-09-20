c69b97185e42ab8f49a929fd3c36506042629e8a
marcelera9 <tch3l0s@gmail.com>
2021-09-20 10:48:53 -0300

    Criando repositorio

---
+++ cmake-build-debug/CMakeFiles/Labirinto.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Labirinto.dir/main.cpp.obj"
+  "Labirinto.exe"
+  "Labirinto.exe.manifest"
+  "Labirinto.pdb"
+  "libLabirinto.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Labirinto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Labirinto.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Labirinto.
+# This may be replaced when dependencies are built.
